de84e5bd0a14ec105be830f48905486a1b2a07ac
EnochZZZZ <zhao4996@gmail.com>
2024-04-27 04:25:53 -0400

    User controller implementation

--- src/main/java/com/example/userserver/entity/User.java
+++ src/main/java/com/example/userserver/entity/User.java
@@ -1,8 +1,13 @@
 package com.example.userserver.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import java.sql.Timestamp;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -14,52 +19,4 @@ public class User {
     private String photoURL;
     private Timestamp createdAt;
     private Timestamp updatedAt;
-
-    public User(String name, String email, String photoURL, Timestamp createdAt, Timestamp updatedAt) {
-        this.name = name;
-        this.email = email;
-        this.photoURL = photoURL;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhotoURL() {
-        return photoURL;
-    }
-
-    public void setPhotoURL(String photoURL) {
-        this.photoURL = photoURL;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Timestamp getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Timestamp updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
